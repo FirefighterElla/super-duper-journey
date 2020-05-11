300fbe0c61343bc7adb12fe1331ef08ff7535b37
FirefighterElla <63886816+FirefighterElla@users.noreply.github.com>
2020-05-11 18:58:55 -0400

    Changed bots purpose
    Thats all

--- bot.js
+++ bot.js
@@ -9,90 +9,105 @@ client.on("ready", () => {
 require('events').EventEmitter.defaultMaxListeners
 =25
 
-  function randomMessage(){
-    var randomNumber = Math.round(Math.random()*2); // 0, 1 or 2
-    switch(randomNumber){
-        case 0: return 'Paper';
-        case 1: return 'Rock';  
-        case 2: return 'Scissors';
-    }
-}
-
-client.on('message', (message)=>{ 
-    if(message.content == "~Rock"){
-        message.reply(randomMessage());
-    }
-});
-
-client.on('message', (message)=>{ 
-  if(message.content == "~rock"){
-      message.reply(randomMessage());
+ client.on("message", (message) => {
+ if (message.content.startsWith(config.prefix + "support")) {
+    message.channel.send("Hey please stay with us. We have <@&700845339313176626> on route to you right now! If no one responds within 5 minutes please use this command again. If you are feeling suicidal please call a crisis hotline listed in the <#701895097951977543> channel. If you are not able to call please use *suicide to get a suicide response squad. If you are having urges use `*urges` and if your struggling to breathe use `*breathing`");
   }
-});
+ });
 
-client.on('message', (message)=>{ 
-  if(message.content == "~Paper"){
-      message.reply(randomMessage());
-  }
-});
-
-client.on('message', (message)=>{ 
-  if(message.content == "~paper"){
-      message.reply(randomMessage());
-  }
-});
-
-client.on('message', (message)=>{ 
-  if(message.content == "~Scissors"){
-      message.reply(randomMessage());
-  }
-});
-
-client.on('message', (message)=>{ 
-  if(message.content == "~scissor"){
-      message.reply(randomMessage());
-  }
-});
-
-client.on('message', (message)=>{ 
-  if(message.content == "~Scissor"){
-      message.reply(randomMessage());
-  }
-});
-
-client.on('message', (message)=>{ 
-  if(message.content == "~scissors"){
-      message.reply(randomMessage());
-  }
-});
+ client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "Support")) {
+     message.channel.send("Hey please stay with us. We have <@&700845339313176626> on route to you right now! If no one responds within 5 minutes please use this command again. If you are feeling suicidal please call a crisis hotline listed in the <#701895097951977543> channel. If you are not able to call please use *suicide to get a suicide response squad. If you are having urges use `*urges` and if your struggling to breathe use `*breathing`");
+   }
+  });
 
 client.on("message", (message) => {
   if (message.content.startsWith(config.prefix + "creator")) {
-    message.channel.send("The creator of the bot was `@-=- Ella -=-#1011` any suggestions please dm her. Copyright 2020");
+    message.channel.send("The creator of the bot was <@683845278175985683> any suggestions please dm her. Copyright 2020");
   }
 });
 
 client.on("message", (message) => {
   if (message.content.startsWith(config.prefix + "Creator")) {
-    message.channel.send("The creator of the bot was `@-=- Ella -=-#1011` any suggestions please dm her. Copyright 2020");
+    message.channel.send("The creator of the bot was <@683845278175985683> any suggestions please dm her. Copyright 2020");
   }
 });
 
   client.on("message", (message) => {
-    if (message.content.startsWith(config.prefix + "help")) {
-      message.channel.send("Here are some commands for the bot:"),
-      message.channel.send("~Wanna play rock paper scissors - Simple, allows you to play rock paper scissors, after that you can respond with: ~rock ~scissors ~paper"),
-      message.channel.send("All commands are not case sensitive.");
+      if (message.content.startsWith(config.prefix + "cutie")) {
+         message.channel.send("<@669680368881893426> is a big cutie");
     }
   });
 
   client.on("message", (message) => {
-    if (message.content.startsWith(config.prefix + "Help")) {
-      message.channel.send("Here are some commands for the bot:"),
-      message.channel.send("Use one of these 3 ~rock ~scissors ~paper and the bot will respond, dont get mad if you loose tho!"),
-      message.channel.send("~creator tells you about the creator of the bot"),
-      message.channel.send("All commands are not case sensitive.");
-    }
-  });
+    if (message.content.startsWith(config.prefix + "Cutie")) {
+       message.channel.send("<@669680368881893426> is a big cutie");
+  }
+});
+
+ client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "crisis")) {
+     message.channel.send("Hey! Please stay with us! Help is on the way ASAP if you require immediate help preferably call one of the crisis hotlines in <#701895097951977543>. <@&709099579521499216>. If you are currently having trouble to breath or have urges use `*breathing` and/or `*urges`. Please stay with us someone will be with you shortly");
+ }
+ });
+
+ client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "Crisis")) {
+     message.channel.send("Hey! Please stay with us! Help is on the way ASAP if you require immediate help preferably call one of the crisis hotlines in <#701895097951977543>. <@&709099579521499216>. If you are currently having trouble to breath or have urges use `*breathing` and/or `*urges`. Please stay with us someone will be with you shortly");
+ }
+ });
+
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "breathing")) {
+     message.channel.send("If you are sturggling to breathe follow the instructions of this gif: https://cdn.discordapp.com/attachments/707376619089690754/708076309749825656/200w.webp");
+}
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "Breathing")) {
+     message.channel.send("If you are sturggling to breathe follow the instructions of this gif: https://cdn.discordapp.com/attachments/707376619089690754/708076309749825656/200w.webp");
+}
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "urges")) {
+     message.channel.send("Hey if you are having urges, follow the advice of this image: https://cdn.discordapp.com/attachments/707376619089690754/708137625336283156/XfxmPdt_7XfRyKwQ18PDPVEmvTuY9DSgxJH7hIrObe8.jpg");
+}
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "Urges")) {
+     message.channel.send("Hey if you are having urges, follow the advice of this image: https://cdn.discordapp.com/attachments/707376619089690754/708137625336283156/XfxmPdt_7XfRyKwQ18PDPVEmvTuY9DSgxJH7hIrObe8.jpg");
+}
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "help")) {
+    message.channel.send("Here are some commands for the bot:"),
+    message.channel.send("The prefix is: '*'"),
+    message.channel.send("*support - gets support for venting"),
+    message.channel.send("*crisis - gets support if you are in the middle of a crisis (panic attack, wating to do self harm, etc)");
+    message.channel.send("*breathing - gives you a breathing gif");
+    message.channel.send("*urges - If you get urges this is what you should do instead");
+    message.channel.send("*creator - Gives you info about the amazing creator");
+    message.channel.send("*cutie - Tells you who the biggest cutie is (aka the other Ella)");
+    message.channel.send("All commands are not case sensitive.");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith(config.prefix + "Help")) {
+    message.channel.send("Here are some commands for the bot:"),
+    message.channel.send("The prefix is: '*'"),
+    message.channel.send("*support - gets support for venting"),
+    message.channel.send("*crisis - gets support if you are in the middle of a crisis (panic attack, wating to do self harm, etc)");
+    message.channel.send("*breathing - gives you a breathing gif");
+    message.channel.send("*urges - If you get urges this is what you should do instead");
+    message.channel.send("*creator - Gives you info about the amazing creator");
+    message.channel.send("*cutie - Tells you who the biggest cutie is (aka the other Ella)");
+    message.channel.send("All commands are not case sensitive.");
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
